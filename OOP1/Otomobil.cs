61a98a8ae93a6149db3f894e601b3be5d30a9643
cezalet <bekirkaratekin_18@hotmail.com>
2024-02-15 11:59:06 +0300

    Add project files.

---
+++ OOP1/Otomobil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class Otomobil
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int ModelYili { get; set; }
+        public string Renk { get; set; }
+        public string VitesTipi { get; set; }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Otomobil oto;
+            //oto= new Otomobil();
+            //oto.Marka = "Renault";
+            //oto.Model = "Clio 4";
+            //oto.ModelYili = 2020;
+            //oto.Renk = "Beyaz";
+            //oto.VitesTipi = "Manuel";
+
+
+            //toplamaYap(5, 7);
+            //Console.WriteLine("İşlemin Sonucu: "+ toplamaYap(5, 7));
+
+            int sayi1;
+            int sayi2;
+            Console.Write("Bir sayı giriniz:");
+            sayi1 = int.Parse(Console.ReadLine());
+            Console.Write("Bir sayı giriniz:");
+            sayi2 = int.Parse(Console.ReadLine());
+            BuyukKucuk(sayi1, sayi2);
+
+
+
+            //TekCift(sayi);
+            //if (KareAl(sayi)<25)
+            //{
+            //    Console.WriteLine("Sayının değeri:"+ KareAl(sayi)+" 25'den küçüktür.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayının değeri:"+ KareAl(sayi)+"  25'den büyüktür.");
+            //}
+
+            Console.ReadLine();
+        }
+        static int toplamaYap(int sayi1, int sayi2)
+        {
+            int sonuc1=sayi1+sayi2;
+            return sonuc1;
+        }
+
+        static int KareAl(int sayi)
+        {
+            return sayi * sayi;
+        }
+
+        static void TekCift(int sayi)
+        {
+            if (sayi%2==0)
+            {
+                Console.WriteLine("Girmiş olduğunuz sayi("+sayi+") çifttir...");
+            }
+            else
+            {
+                Console.WriteLine("Girmiş olduğunuz sayi(" + sayi + ") tektir...");
+            }
+        }
+        static void BuyukKucuk(int sayi1,int sayi2)
+        {
+            if (sayi1>sayi2)
+            {
+                Console.WriteLine("Birinci sayı("+sayi1+") ikinci sayıdan("+sayi2+") büyüktür.");
+            }
+            else if (sayi1 < sayi2)
+            {
+                Console.WriteLine("Birinci sayı(" + sayi1 + ") ikinci sayıdan(" + sayi2 + ") küçüktür.");
+            }
+            else
+            {
+                Console.WriteLine("Birinci sayı(" + sayi1 + ") ikinci sayıya(" + sayi2 + ") eşittir.");
+            }
+        }
+    }
+}
